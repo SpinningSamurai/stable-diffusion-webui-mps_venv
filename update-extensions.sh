984ed0b0f47a74a3a6b4a6f9088034d3846c778d
SpinningSamurai <97383975+SpinningSamurai@users.noreply.github.com>
2022-12-06 17:01:26 +0900

    Create update-extensions.sh
    Made for batch update of extensions

---
+++ update-extensions.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+dir_path="/Users/enter_your_username_here/stable-diffusion-project/stable-diffusion-webui/extensions/*"
+
+for dir in $dir_path;
+do
+  (
+    echo $dir
+    cd $dir
+    git pull
+  )
+done
